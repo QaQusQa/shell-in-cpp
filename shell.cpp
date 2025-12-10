16d29db50651a14f926d6f0f3b3c6f5a784ed2d1
QaQusQa <qaqaqushayyiq@gmail.com>
2025-12-10 21:18:55 +0700

    first init

---
+++ shell.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <unistd.h>
+#include <string>
+
+int main()
+{
+    std::cout << "$ ";
+
+    std::string command;
+    std::getline(std::cin, command);
+    
+}
