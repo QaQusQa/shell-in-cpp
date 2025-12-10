44bcb833dbf73ef5e08d88e7e895bcd72304ee18
QaQusQa <qaqaqushayyiq@gmail.com>
2025-12-10 21:37:53 +0700

    add type

--- shell.cpp
+++ shell.cpp
@@ -7,9 +7,39 @@
 
 int main()
 {
-    std::cout << "$ ";
+    while (true)
+    {
+        std::cout << "$ ";
 
-    std::string command;
-    std::getline(std::cin, command);
-    
+        std::string perintah;
+        std::getline(std::cin, perintah);
+
+        if (perintah.empty())
+        {
+            continue;
+        }
+        else if (perintah.find("type ") == 0)
+        {
+            std::vector<std::string> listPerintah = {"exit", "echo", "cd", "mkdir", "touch", "type"};
+            std::string tipePerintah = perintah.substr(5);
+
+            auto cariDiPerintah = std::find(listPerintah.begin(), listPerintah.end(), tipePerintah);
+            if (cariDiPerintah != listPerintah.end())
+            {
+                std::cout << tipePerintah << " is a shell builtin" << std::endl;
+            }
+            else
+            {
+                std::cout << "bash: type: " << tipePerintah << ": not found" << std::endl;
+            }
+        }
+        else if (perintah == "exit")
+        {
+            break;
+        }
+        else
+        {
+            std::cout << perintah << ": not found" << std::endl;
+        }
+    }
 }
